1261bb9ccfe95e548232b15a8b3d8be602abe320
XXX-Stalker <2931727685@qq.com>
2025-07-29 16:54:02 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,8 @@
-from ServerCode.Server import Server
+import sys
+import os
 from ServerCode.init import init
+
+script_path = os.path.abspath(__file__)
 
 title = """
  ███████████     ███████    ███████████ ██████   █████ ██████████ ███████████             ████████ 
@@ -15,7 +18,22 @@ XXX-STALKER
 """
 
 if __name__ == "__main__":
+    with open("script_path.txt", 'w') as file:
+        file.write(script_path)
     init()
-    print(title)
-    server = Server(host='127.0.0.1', port=1000)
+    with open("ListenIP_PORT.txt", 'r') as file:
+        address = file.read().strip()
+        print(f"\n连接监听地址：{address}")
+        if ':' in address:
+            host, port_str = address.split(':', 1)
+            try:
+                port = int(port_str)
+            except ValueError:
+                print(f"错误：端口号必须为整数，当前为 '{port_str}'")
+                sys.exit(1)
+        else:
+            print(f"错误：文件格式应为 'IP:PORT'，当前为 '{address}'")
+            sys.exit(1)
+    from ServerCode.Server import Server
+    server = Server(host=host, port=port)
     server.root.mainloop()
